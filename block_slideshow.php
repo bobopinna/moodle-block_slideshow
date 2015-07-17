8a3a3f9e8dfe32eecae9a5a72f50454362dab066
Roberto Pinna <bobo@di.unipmn.it>
2015-07-17 14:10:09 +0200

    Added slide deletion by cleaning image,title, caption and link fields in edit page

--- block_slideshow.php
+++ block_slideshow.php
@@ -102,8 +102,11 @@ class block_slideshow extends block_base {
                     }
 
                     if (!empty($this->config->title[$i]) || !empty($this->config->caption[$i]) || !empty($imagefile)) {
-                        $slidestyle = ($i == $this->config->firstslide)?'display: block; position:static; width: 100%':'width: 100%';
-                        $slidestext .= html_writer::start_tag('div', array('class' => 'block_slideshow_slide', 'style' => $slidestyle));
+                        $slidestyle = ($i == $this->config->firstslide)
+                                      ?'display: block; position:static; width: 100%'
+                                      :'width: 100%';
+                        $slidestext .= html_writer::start_tag('div', array('class' => 'block_slideshow_slide',
+                                                                                      'style' => $slidestyle));
                         if (!empty($imagefile)) {
                             $imagestyle = 'width: 100%';
                             if (isset($this->config->imageposition[$i])) {
@@ -122,12 +125,18 @@ class block_slideshow extends block_base {
                                     break;
                                 }              
                             }
-                            $imageurl = moodle_url::make_pluginfile_url($this->context->id,'block_slideshow', 'slides', $i, $imagefile->get_filepath(), $imagefile->get_filename());
+                            $imageurl = moodle_url::make_pluginfile_url($this->context->id, 
+                                                                        'block_slideshow',
+                                                                        'slides',
+                                                                        $i,
+                                                                        $imagefile->get_filepath(),
+                                                                        $imagefile->get_filename());
                             $imagetext = html_writer::empty_tag('img', array('src' => $imageurl));
                             if (!empty($this->config->link[$i])) {
                                 $imagetext = html_writer::link(new moodle_url($this->config->link[$i]),  $imagetext);
                             }     
-                            $slidestext .= html_writer::tag('div',$imagetext, array('class' => 'block_slideshow_image', 'style' => $imagestyle));
+                            $slidestext .= html_writer::tag('div',$imagetext, array('class' => 'block_slideshow_image',
+                                                                                    'style' => $imagestyle));
                         }
                         if (!empty($this->config->title[$i]) || !empty($this->config->caption[$i])) {
                             $captionstyle = 'bottom: 1em; right: 1em';
@@ -166,7 +175,8 @@ class block_slideshow extends block_base {
                                 }
                             }
 
-                            $slidestext .= html_writer::start_tag('div', array('class' => 'block_slideshow_caption', 'style' => $captionstyle));
+                            $slidestext .= html_writer::start_tag('div', array('class' => 'block_slideshow_caption',
+                                                                               'style' => $captionstyle));
                             if (!empty($this->config->title[$i])) {
                                 $titletext = $this->config->title[$i];
                                 if (!empty($this->config->link[$i])) {
@@ -191,7 +201,8 @@ class block_slideshow extends block_base {
 
             if ($slidesactive) {
                 $this->content->text .= html_writer::start_tag('div', array('class' => 'block_slideshow_slideshow'));
-                $this->content->text .= html_writer::start_tag('div', array('id' => 'block_slideshow_'.$ssid, 'class' => 'block_slideshow_slides'));
+                $this->content->text .= html_writer::start_tag('div', array('id' => 'block_slideshow_'.$ssid,
+                                                                            'class' => 'block_slideshow_slides'));
                 $this->content->text .= $slidestext;
                 $this->content->text .= html_writer::end_tag('div');
                 if ($slidesactive > 1) {
@@ -203,26 +214,44 @@ class block_slideshow extends block_base {
                     switch ($transition) {
                         case 'fade':
                         case 'slideLeft':
-                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/left', get_string('prev')), array('id' => 'block_slideshow_prev_'.$ssid, 'class' => 'block_slideshow_left'));
-                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/right', get_string('next')), array('id' => 'block_slideshow_next_'.$ssid, 'class' => 'block_slideshow_right'));
+                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/left', get_string('prev')),
+                                                                     array('id' => 'block_slideshow_prev_'.$ssid,
+                                                                           'class' => 'block_slideshow_left'));
+                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/right', get_string('next')),
+                                                                     array('id' => 'block_slideshow_next_'.$ssid,
+                                                                           'class' => 'block_slideshow_right'));
 			break;
                         case 'slideRight':
-                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/left', get_string('next')), array('id' => 'block_slideshow_next_'.$ssid, 'class' => 'block_slideshow_left'));
-                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/right', get_string('prev')), array('id' => 'block_slideshow_prev_'.$ssid, 'class' => 'block_slideshow_right'));
+                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/left', get_string('next')),
+                                                                     array('id' => 'block_slideshow_next_'.$ssid,
+                                                                           'class' => 'block_slideshow_left'));
+                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/right', get_string('prev')),
+                                                                     array('id' => 'block_slideshow_prev_'.$ssid,
+                                                                           'class' => 'block_slideshow_right'));
                         break;
                         case 'slideUp':
-                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/up', get_string('prev')), array('id' => 'block_slideshow_prev_'.$ssid, 'class' => 'block_slideshow_up'));
-                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/down', get_string('next')), array('id' => 'block_slideshow_next_'.$ssid, 'class' => 'block_slideshow_down'));
+                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/up', get_string('prev')),
+                                                                     array('id' => 'block_slideshow_prev_'.$ssid,
+                                                                           'class' => 'block_slideshow_up'));
+                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/down', get_string('next')),
+                                                                     array('id' => 'block_slideshow_next_'.$ssid,
+                                                                           'class' => 'block_slideshow_down'));
 			break;
                         case 'slideDown':
-                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/up', get_string('next')), array('id' => 'block_slideshow_next_'.$ssid, 'class' => 'block_slideshow_up'));
-                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/down', get_string('prev')), array('id' => 'block_slideshow_prev_'.$ssid, 'class' => 'block_slideshow_down'));
+                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/up', get_string('next')),
+                                                                     array('id' => 'block_slideshow_next_'.$ssid,
+                                                                           'class' => 'block_slideshow_up'));
+                            $this->content->text .= html_writer::tag('div', $OUTPUT->pix_icon('t/down', get_string('prev')),
+                                                                     array('id' => 'block_slideshow_prev_'.$ssid,
+                                                                           'class' => 'block_slideshow_down'));
                         break;
                     }
-                    $this->content->text .= html_writer::start_tag('ul', array('class' => 'block_slideshow_pages', 'id' => 'block_slideshow_pages_'.$ssid));
+                    $this->content->text .= html_writer::start_tag('ul', array('class' => 'block_slideshow_pages',
+                                                                               'id' => 'block_slideshow_pages_'.$ssid));
                     for ($i=0; $i<$slidesactive; $i++) {
                         $firstslide = ($i == $this->config->firstslide)?' yui3-slideshow-active':'';
-                        $this->content->text .= html_writer::empty_tag('li', array('class' => 'block_slideshow_page block_slideshow_page_'.$ssid.$firstslide.' '.$transition));
+                        $this->content->text .= html_writer::empty_tag('li', 
+                                 array('class' => 'block_slideshow_page block_slideshow_page_'.$ssid.$firstslide.' '.$transition));
                     }
                     $this->content->text .= '</ul>';
 
@@ -246,29 +275,62 @@ class block_slideshow extends block_base {
      */
 
     function instance_config_save($data, $nolongerused = false) {
-        global $COURSE;
+        global $USER, $COURSE;
 
-        $config = clone($data);
+        $config = new stdClass();
+        foreach ($data as $fieldname => $fieldvalue) {
+             if (is_array($fieldvalue)) {
+                 $config->{$fieldname} = array();
+             } else {
+                 $config->{$fieldname} = $fieldvalue;
+             }
+        }
         $fileoptions = array('subdirs'=>false,
                              'maxfiles'=>1,
                              'maxbytes'=>$COURSE->maxbytes,
                              'accepted_types'=>'web_image',
                              'return_types'=>FILE_INTERNAL);
 
-        $removed = 0;
-        for ($i=0; $i<$config->slides; $i++) {
-            if (!empty($config->title[$i]) || !empty($config->caption[$i]) || !empty($config->link[$i])) {
-                file_save_draft_area_files($config->imageslide[$i], $this->context->id, 'block_slideshow', 'slides',  $i, $fileoptions);
-            } else {
-                unset($config->enabled[$i]);
-                unset($config->imageslide[$i]);
-                unset($config->title[$i]);
-                unset($config->caption[$i]);
-                unset($config->link[$i]);
-                $removed++;
+        $saved = 0;
+        for ($i=0; $i<$data->slides; $i++) {
+            $usercontext = context_user::instance($USER->id);
+            $fs = get_file_storage();
+            $draftfiles = $fs->get_area_files($usercontext->id, 'user', 'draft', $data->imageslide[$i], 'id');
+            if ((count($draftfiles) > 1)
+                   ||(isset($data->title[$i]) && !empty($data->title[$i]))
+                   || (isset($data->caption[$i]) && !empty($data->caption[$i]))
+                   || (isset($data->link[$i]) && !empty($data->link[$i]))) {
+
+                $config->enabled[$saved] = 0;
+                if (isset($data->enabled[$i]) && !empty($data->enabled[$i])) {
+                    $config->enabled[$saved] = $data->enabled[$i];
+                    if ($data->firstslide >= $saved) {
+                        $config->firstslide = $saved;
+                    }
+                }
+                if (isset($data->imageslide[$i]) && !empty($data->imageslide[$i])) {
+                    $config->imageslide[$saved] = $data->imageslide[$i];
+                    file_save_draft_area_files($config->imageslide[$saved],
+                                               $this->context->id,
+                                               'block_slideshow',
+                                               'slides',
+                                               $saved,
+                                               $fileoptions);
+                }
+                $config->imageposition[$saved] = isset($data->imageposition[$i]) && !empty($data->imageposition[$i])
+                                                 ?$data->imageposition[$i]
+                                                 :'';
+                $config->title[$saved] = isset($data->title[$i]) && !empty($data->title[$i])?$data->title[$i]:'';
+                $config->caption[$saved] = isset($data->caption[$i]) && !empty($data->caption[$i])?$data->caption[$i]:'';
+                $config->captionposition[$saved] = isset($data->captionposition[$i]) && !empty($data->captionposition[$i])
+                                                   ?$data->captionposition[$i]
+                                                   :'';
+                $config->link[$saved] = isset($data->link[$i]) && !empty($data->link[$i])?$data->link[$i]:'';
+                $saved++;
+                
             }
         }
-        $config->slides = $config->slides - $removed;
+        $config->slides = $saved;
 
         parent::instance_config_save($config, $nolongerused);
     }
