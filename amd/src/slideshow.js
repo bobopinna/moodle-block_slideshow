ff9b7223896f64b2b86ab00a465429def54738bc
Roberto Pinna <bobo@di.unipmn.it>
2018-08-10 16:36:27 +0200

    Grunted amd package

--- amd/src/slideshow.js
+++ amd/src/slideshow.js
@@ -1,10 +1,8 @@
-define(['jquery', 'block_slideshow/slick'], function($, c) {
+define(['jquery', 'block_slideshow/slick'], function($) {
     return {
         init: function ($params) {
-            //fontsize = $('.block_slideshow_slideshow').width()/$(document).width();
-            //$('.block_slideshow_slideshow').css("font-size", fontsize + "vw");
             $(window).resize(function() {
-                fontsize = $('.block_slideshow_slideshow').width()/$(document).width();
+                var fontsize = $('.block_slideshow_slideshow').width()/$(document).width();
                 $('.block_slideshow_slideshow').css("font-size", fontsize + "vw");
                 if (parseFloat($('.block_slideshow_slideshow').css("font-size")) < 9) {
                    $('.block_slideshow_slideshow').css("font-size", "9px");
